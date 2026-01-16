10b326c0eac9fb2d75ff4ebd7b3daee2695c9798
tituvai <titu17940@gmail.com>
2026-01-16 13:20:09 +0600

    add otp

--- route/api/auth/registacion.js
+++ route/api/auth/registacion.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const signupController = require('../../../controllers/signupController')
+const otpController = require('../../../controllers/otpController')
 const router = express.Router()
 
 router.post('/signup', signupController)
+router.post('/verified', otpController)
 
 
 module.exports = router
